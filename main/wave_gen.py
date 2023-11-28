c6d9fd63c5ab85df384346e25328060a08e93d5d
Andy Whitworth <awhitworth1990@gmail.com>
2023-11-28 10:58:16 +0000

    added baseline model and started wave gen function

--- main/wave_gen.py
+++ main/wave_gen.py
@@ -1,27 +1,60 @@
 import numpy as np
 
 
-def generate_wave_train(Hs, Tp, no_freq, seed):
+def generate_wave_train(Hs, Tp, seed, N=100, r_min=0.5, r_max=10,
+                        sim_time=60, time_step=0.05):
     
-    if Tp/np.sqrt(Hs) <= 3.6: gamma = 5
-    elif Tp/np.sqrt(Hs) >= 5: gamma = 1
-    else: gamma = np.exp(5.75-1.15*Tp/np.sqrt(Hs))
+    np.random.seed(seed)
+    
+    fp = 1/Tp
+    
+    f_min = r_min/Tp
+    f_max = r_max/Tp
+    f_step = (f_max - f_min)/N
+    
+    freqs = np.arange(f_min+f_step/2,f_max,f_step)
+    spectrum = 5/16 * Hs**2 * fp**4 * freqs**-5 * np.exp(-5/4 * (fp/freqs)**4)
+    wave_amps = (2 * spectrum * (freqs[0:] - np.concatenate(([0],freqs))[0:-1]))**0.5
+    wave_phases = np.random.rand(N) * 2*np.pi
     
     
+    #decide how to load in raos and periods
+    #pass to function? nerrrr
+    #load in numpy arrays? not sure
+    #load in lis file? need seperate function, might be messy
+    #do i have numpy arrays for all those vessels saved? this?
+        #use global variable for location and name?
     
-    start_freq = 0.05 #needs to be funciton of tp
-    end_freq = 1. #needs to be funciton of tp
+    surge_amp, surge_ph = interpolarte_raos(1/periods, surge_raw_amp, surge_raw_ph, freqs)
+    heave_amp, heave_ph = interpolarte_raos(1/periods, heave_raw_amp, heave_raw_ph, freqs)
+    pitch_amp, pitch_ph = interpolarte_raos(1/periods, pitch_raw_amp, pitch_raw_ph, freqs)
     
-    frequencies = np.linspace(start_freq,end_freq,no_freq) #maybe log space?
-    amplitudes = 
+    wave_train = []
+    surge = [] 
+    heave = []
+    pitch = []
+    for t in np.arange(0,sim_time, time_step):
+        if t < 15:
+            ramp = t/15
+        else:
+            ramp = 1.0
+        
+        surge_components = ramp * wave_amps * surge_amp * np.cos((surge_ph - wave_phases) - 2*np.pi*freqs*t)
+        heave_components = ramp * wave_amps * heave_amp * np.cos((heave_ph - wave_phases) - 2*np.pi*freqs*t)
+        pitch_components = ramp * wave_amps * pitch_amp * np.cos((pitch_ph - wave_phases) - 2*np.pi*freqs*t)
+        
+        wave_components = ramp * wave_amps * np.cos(2*np.pi*freqs*t - wave_phases)
+        wave_train.append(sum(wave_components))
+        surge.append(sum(surge_components))
+        heave.append(sum(heave_components))
+        pitch.append(sum(pitch_components))
+        
     
-    wave_train = 
-    
-    return wave_train
+    return wave_train, surge, heave, pitch
 
-def generate_motions(wave_train, rao):
+def interpolarte_raos(in_freqs, raw_amp, raw_phase, out_freq):
     
-    return motion
+    return amplitude, phase
     
     
     
