45d2cb658b71c272fbc76498e72f2c0ad530c157
Jules <juleslockey2003@gmail.com>
2023-11-29 17:21:06 +0000

    updated render function with correct camera positioning and lighting to enhance video

--- render_create_video.py
+++ render_create_video.py
@@ -7,23 +7,26 @@ import bpy
 
 
 def render_images():
+    """
+    Calls the animated ship function and produces a set of images based on each frame
+    produced throughout the model. It then saves each individual image into a seperate
+    folder to be called again later.
+    """
 
     animate_ship()
 
-    bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
-
-    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 0), rotation=(1.44513, 1.647e-07, 0.502655), scale=(1, 1, 1))
-
+    # Creates a camera and positions the camera at a point to accurately view the object
     cam = bpy.data.objects['Camera']
-    x = 18
+    x = 22
     y = -30
-    z = 30
-    rx = -0.5
-    ry = 0.8
-    rz = -1.3
+    z = 10
+    rx = 1.38
+    ry = 0.18
+    rz = 0.59
     cam.location = (x,y,z)
     cam.rotation_euler  = (rx,ry,rz)
 
+    # Renders the images at set values and saves these images in a new folder
     for scene in bpy.data.scenes:
         scene.render.ffmpeg.codec = 'FFV1'
         scene.render.fps = 24
@@ -32,45 +35,52 @@ def render_images():
         scene.render.filepath = '/Users/juleslockey/code/AndyW1990/rl-control/model_3d/test_images/test'
         bpy.ops.render.render(animation=True)
 
+    lamp_data = bpy.data.lights.new(name="Lighting", type='SUN')
 
+    # Create new object with our lamp datablock
+    lamp_object = bpy.data.objects.new(name="Lighting", object_data=lamp_data)
 
+    # Place lamp to a specified location
+    lamp_object.location = (5.0, 5.0, 5.0)
 
-
+    # And finally select it make active
+    # scene.objects.active = lamp_object
 
 def generate_video():
+    """
+    Takes in all the image files in a ceration folder and from there, merges all of the images
+    in order to produce a video at a set frame rate that is saved in the same folder as
+    where the images are saved.
+    """
+
+    # Sets the file locations and video name to be saved
     image_folder = '/Users/juleslockey/code/AndyW1990/rl-control/model_3d/test_images' # make sure to use your folder
     video_name = 'test_vid.mp4'
     os.chdir("/Users/juleslockey/code/AndyW1990/rl-control/model_3d/test_images")
 
+    # Takes out all of the images saved in the given folder and saves them in a list before sorting them
     images = [img for img in os.listdir(image_folder)
               if img.endswith(".jpg") or
                  img.endswith(".jpeg") or
                  img.endswith("png")]
-
     images = sorted(images)
 
-    # Array images should only consider
-    # the image files ignoring others if any
-
+    # Takes in the height and width of the first image to make sure later on that all images are of the same size
     frame = cv2.imread(os.path.join(image_folder, images[0]))
-
-    # setting the frame width, height width
-    # the width, height of first image
     height, width, layers = frame.shape
 
+    # Sets up the video paramaters, such as the frame rate, the type of video and the size
     fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     video = cv2.VideoWriter(video_name, fourcc, 24, (width, height))
 
-    # Appending the images to the video one by one
+    # For each image, it writes the image sequentially into the video to produce the video
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
 
-    # Deallocating memories taken for window creation
-    # cv2.destroyAllWindows()
-    video.release()  # releasing the video generated
+    # 'Releases' the video, meaning it now knows the video is finished and pushes it back to the folder
+    video.release()
 
-# motion_setup()
-
+# Calling the render images before the video creation
 render_images()
 
 # Calling the generate_video function
