51f5ddfbbd3c5d91ade8a1562f4abfd747ad1859
Ben Edwards <ben.edwards101@hotmail.co.uk>
2023-11-29 13:53:48 +0000

    Changed the py files with instructions

---
+++ rl_control/main.py
@@ -0,0 +1,5 @@
+from rl_control import agent, environment
+
+
+'''Initialize our classes and create our agent using import.
+   Then run the learning process interaction between the agent and the environment '''
---
+++ rl_control/network.py
@@ -0,0 +1,7 @@
+# Define the neural network to be used.
+
+"""
+Neural network module.
+
+This will define the architecture used by the agent classes
+"""
